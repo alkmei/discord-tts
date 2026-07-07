d34ee638b86998fefc19054bc4591a17c4138cef
Alvin Mei <alvinkmei@gmail.com>
2026-07-06 23:41:11 -0400

    SECURE_PROXY_SSL_HEADER added

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,7 @@ if not DEBUG:
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 else:
     SECRET_KEY = "dev-key-keep-it-secret"
     ALLOWED_HOSTS = ["*"]
